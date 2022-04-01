1ed965a7cfa83583b60e2b5dffa7211d64b37f93
ilja615 <615ilja@gmail.com>
2022-04-01 10:30:46 +0200

    idk i tried something with repositorysource but didnt work

--- src/main/java/ilja615/expedite/ExpediteMod.java
+++ src/main/java/ilja615/expedite/ExpediteMod.java
@@ -3,9 +3,13 @@ package ilja615.expedite;
 import ilja615.expedite.init.ModBlocks;
 import ilja615.expedite.init.ModItems;
 import ilja615.expedite.init.ModProperties;
+import ilja615.expedite.util.ExpediteRepositorySource;
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.repository.*;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -15,8 +19,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.io.File;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 import static ilja615.expedite.ExpediteMod.MOD_ID;
 
@@ -76,6 +85,11 @@ public class ExpediteMod
         @SubscribeEvent
         public static void entityAttributes(final EntityAttributeCreationEvent event) {
             //ModEntities.CreateEntityAttributes(event); //It creates entity attributes
+        }
+
+        @SubscribeEvent
+        public static void addPackFinder(final AddPackFindersEvent event) {
+            event.addRepositorySource(new ExpediteRepositorySource(FMLPaths.MODSDIR.get().resolve("expedite")));
         }
     }
 }
